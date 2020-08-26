327c4d2d507aa2ef538ef30f8b62790cf362bdce
Ian Gunter <iangunterofamerica@yahoo.com>
2020-08-25 23:33:55 -0600

    Add project files.

---
+++ SayMyNameAgain.vb
@@ -0,0 +1,23 @@
+﻿'Ian Gunter
+'RCET0265
+'Spring 2020
+'Say My Name Again
+
+Option Explicit On
+Option Strict On
+
+Module SayMyNameAgain
+
+    Sub Main()
+        'Dim assigns the next work to whatever As is set as. 
+        Dim Input As String
+        Console.WriteLine("What Is your name")
+        Input = Console.ReadLine()
+        If Input = "Emily" Then Console.WriteLine("Wow " & Input & "! Thats an awesome name!")
+        If Input = "Joe" Then Console.WriteLine("Wow " & Input & "! Thats an awesome name!")
+        If Input = "Ian" Then Console.WriteLine("Oh, I've heard of you! " & Input & " The G!! What a pleasure to meet you.")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
